9033fa6b1c06925b3206fbf99589208332a9f9dc
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2025-08-16 22:20:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,88 +2,123 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-
-import re
 import os
+import re
 from os import environ
 from Script import script
 
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
+# Matches optional leading minus and digits (Telegram IDs can be negative for channels)
+id_pattern = re.compile(r'^-?\d+$')
+
+def is_enabled(value, default=False):
+    """Parse a variety of truthy/falsey strings into a boolean."""
+    if value is None:
         return default
-      
-# Bot Information
-API_ID = int(environ.get("API_ID", "4770590"))
+    v = str(value).strip().lower()
+    if v in {"true", "yes", "1", "enable", "y", "on"}:
+        return True
+    if v in {"false", "no", "0", "disable", "n", "off"}:
+        return False
+    return default
+
+def to_int(value, default=0):
+    try:
+        return int(value)
+    except Exception:
+        return default
+
+
+# -----------------------
+# Bot basic info
+# -----------------------
+API_ID = to_int(environ.get("API_ID", "4770590"))
 API_HASH = environ.get("API_HASH", "e33bf9032335b874acb9c6406f044836")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7184578061:AAEDa_nHSiEzRXFFPIM3UnLkLDOgRhkiXig")
-
-PICS = (environ.get('PICS', 'https://telegra.ph/file/eb119179b4d2a13e71163.jpg https://telegra.ph/file/dc5a1a49c2786685ff97a.jpg https://telegra.ph/file/1a9519a68c4d45ac9455a.jpg https://telegra.ph/file/e5a3d6969f2082eecc3c1.jpg https://telegra.ph/file/57d6d774cf4baf2de5968.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1654334233').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "VenomStoneFileStoreBot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "VenomStoneFileStoreBot")  # without @
 PORT = environ.get("PORT", "8080")
 
-# Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+# -----------------------
+# Pictures / branding
+# -----------------------
+# PICS env can be space-separated URLs. Ensure non-empty list.
+_raw_pics = environ.get('PICS', 'https://telegra.ph/file/eb119179b4d2a13e71163.jpg')
+PICS = [p for p in re.split(r'\s+', _raw_pics.strip()) if p]
 
-# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://sititac651:eRDCYID2OUkqRl0W@cluster0.sf7ffle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-CDB_NAME = environ.get("CDB_NAME", "cluster0")
+if not PICS:
+    PICS = ['https://telegra.ph/file/eb119179b4d2a13e71163.jpg']
 
-# Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://todilinye:KBX8pJW4rvfoFkfM@cluster0.qrwrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# -----------------------
+# Admins
+# -----------------------
+ADMINS_RAW = environ.get('ADMINS', '1654334233').strip()
+ADMINS = []
+if ADMINS_RAW:
+    for admin in ADMINS_RAW.split():
+        admin = admin.strip()
+        if id_pattern.match(admin):
+            ADMINS.append(int(admin))
+        else:
+            ADMINS.append(admin)
+
+# -----------------------
+# Clone / DB
+# -----------------------
+CLONE_MODE = is_enabled(environ.get('CLONE_MODE', "false"))
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
+CDB_NAME = environ.get("CDB_NAME", "")
+
+DB_URI = environ.get(
+    "DB_URI",
+    "mongodb+srv://todilinye:KBX8pJW4rvfoFkfM@cluster0.qrwrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
 DB_NAME = environ.get("DB_NAME", "cluster0")
 
-# Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+# -----------------------
+# Auto Delete
+# -----------------------
+AUTO_DELETE_MODE = is_enabled(environ.get('AUTO_DELETE_MODE', "false"))
+AUTO_DELETE = to_int(environ.get("AUTO_DELETE", "30"))          # minutes
+AUTO_DELETE_TIME = to_int(environ.get("AUTO_DELETE_TIME", "1800"))  # seconds
 
-# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+# -----------------------
+# Channels / Logging
+# -----------------------
+LOG_CHANNEL = to_int(environ.get("LOG_CHANNEL", "-1002113810572"))
 
-# Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002113810572"))
-
-# File Caption Information
+# -----------------------
+# File captions
+# -----------------------
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
-# Enable - True or Disable - False
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+# -----------------------
+# Feature toggles (boolean safe parsing)
+# -----------------------
+PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "true"))
+VERIFY_MODE = is_enabled(environ.get('VERIFY_MODE', "false"))
+WEBSITE_URL_MODE = is_enabled(environ.get('WEBSITE_URL_MODE', "false"))
+STREAM_MODE = is_enabled(environ.get('STREAM_MODE', "false"))
 
-# Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
+# -----------------------
+# Verify / Shortlink / Misc
+# -----------------------
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "")
+SHORTLINK_API = environ.get("SHORTLINK_API", "")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "")
 
-# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "get2short.com") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "9ae188a4e5609c6f089d21d01816974230a64218") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/VenomStoneNetwork/85") # how to open link 
+# -----------------------
+# Stream config & other ints
+# -----------------------
+MULTI_CLIENT = is_enabled(environ.get("MULTI_CLIENT", "false"))
+SLEEP_THRESHOLD = to_int(environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = to_int(environ.get("PING_INTERVAL", "1200"))
+URL = environ.get("URL", "")
 
-# Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+# -----------------------
+# Environment detection
+# -----------------------
+ON_HEROKU = 'DYNO' in environ
 
-# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
-
-# File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
-
-# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-MULTI_CLIENT = False
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-    
+# ---------------------------------------------------------------------
+# End of config
+# ---------------------------------------------------------------------
